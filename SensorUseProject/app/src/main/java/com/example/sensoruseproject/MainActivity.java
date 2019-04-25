1d35c35c243705473b49ccffbf6e6fa2e85df6ec
vijay <vijaytahelramani999@gmail.com>
2019-04-25 16:22:33 -0400

    Sensor use for step count

---
+++ SensorUseProject/app/src/main/java/com/example/sensoruseproject/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.sensoruseproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sm;
+    TextView mySteps;
+    boolean running = false;
+    Sensor countSensor, AccelSensor;
+    private int steps = 0;
+    private float mAccelCurrent; // current acceleration including gravity
+    private float mAccelLast;
+    private float mAccel;
+    float current_steps = 0.0f;
+    boolean shake = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mySteps = (TextView) findViewById(R.id.steps);
+        sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        countSensor = sm.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        AccelSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccel = 0.00f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this,AccelSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        if(countSensor != null){
+            running = true;
+            sm.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
+            Toast.makeText(this,"Sensor Found",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this,"Sensor Not available",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //running = false;
+        sm.unregisterListener(this,AccelSensor);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+            if (mAccel > 12) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Device has shaken.", Toast.LENGTH_LONG);
+                toast.show();
+                current_steps = current_steps + Float.parseFloat(mySteps.getText().toString());
+            }
+        }
+        else if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+
+            if (running) {
+                mySteps.setText(String.valueOf(event.values[0]-current_steps));
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
